f5fb84250689471357593d2263ea19270c306ae2
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-11-15 16:13:04 +0800

    fix exports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,5 @@
 export * from "./useCoinMetadataMap";
+export * from "./useIsAndroid";
+export * from "./useIsiOS";
+export * from "./useIsTouchscreen";
 export * from "./useRefreshOnBalancesChange";
