b6e7502c97471b63801fae09dcaec1ffbbba16e0
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-12-13 12:29:35 +0800

    attempt msafe fix

--- frontend-sui/src/lib/msafe.ts
+++ frontend-sui/src/lib/msafe.ts
@@ -1,5 +1,7 @@
 export const isInMsafeApp = () =>
   typeof window !== "undefined" &&
+  window?.location?.ancestorOrigins &&
+  Array.from(window.location.ancestorOrigins).length > 0 &&
   Array.from(window.location.ancestorOrigins)[0]?.includes("m-safe.io");
 
 export const getMsafeBaseUrl = () =>
